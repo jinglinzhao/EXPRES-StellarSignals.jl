26b92b81c394bab3731a61767f8c9c241b12f380
Eric Ford <ebf11@psu.edu>
2021-02-11 10:13:58 -0500

    Scripts for making CCFs and line-by-line fits for round 2

---
+++ submissions/2021feb15/line_by_line_fits_10700.jl
@@ -0,0 +1,5 @@
+using Pkg
+Pkg.activate(".")
+cd("submissions/2021feb15")
+starid = 10700
+include("line_by_line_fits_generic.jl")
---
+++ submissions/2021feb15/line_by_line_fits_34411.jl
@@ -0,0 +1,5 @@
+using Pkg
+Pkg.activate(".")
+cd("submissions/2021feb15")
+starid = 34411
+include("line_by_line_fits_generic.jl")
